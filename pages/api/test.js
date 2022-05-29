d3286a1a19aa372840e522c3c321e7f0bc348453
tulusibrahim <tulusibrahim20@gmail.com>
2022-05-29 17:22:21 +0700

    test cronjob -4

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,13 @@
 export default function handler(req, res) {
-    var cron = require('node-cron');
-
-    cron.schedule('* * * * *', () => {
-        console.log('running a task every minute');
-    });
+    var CronJob = require('cron').CronJob;
+    var job = new CronJob(
+        '* * * * * *',
+        function () {
+            console.log('You will see this message every second');
+        },
+        null,
+        true,
+        'America/Los_Angeles'
+    );
     res.send({ message: 'success!' })
 }
