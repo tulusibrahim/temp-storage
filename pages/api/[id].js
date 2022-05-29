a5364f98ea2840b51fab8a19e11f91653f3b2768
tulusibrahim <tulusibrahim20@gmail.com>
2022-05-29 17:34:51 +0700

    test cronjob -5

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -8,46 +8,45 @@ export default function handler(req, res) {
   let imageID = req.query.id
   console.log(imageID)
 
-  const deleteWrapper = async (imageID) => {
+  deleteWrapper(imageID)
+
+  res.json({ message: 'success!' })
+}
+
+const deleteWrapper = (imageID) => {
+  setTimeout(async () => {
     let deleteFromStorage = await deleteImageFromStorage(imageID)
     let deleteFromDB = await deleteImageReferenceFromDB(imageID)
     console.log('deleteFromStorage', deleteFromStorage)
     console.log('deleteFromDB', deleteFromDB)
     return deleteFromDB
-  }
-
-  const deleteImageFromStorage = async (fileName, retry = 3) => {
-    let deleteImage = await supabase.storage.from(storageName).remove([fileName])
-    // console.log(deleteImage)
-
-    if (deleteImage.error) {
-      if (retry > 0) return deleteImageFromStorage(fileName, retry - 1)
-      console.log('Failed to delete image from storage')
-    }
-    else {
-      console.log('success delete image from storage')
-      return true
-    }
-  }
-
-  const deleteImageReferenceFromDB = async (imageID, retry = 3) => {
-    let deleteImageReference = await supabase.from('temp_storage').delete().eq('file_name', imageID)
-    // console.log(deleteImageReference)
-
-    if (deleteImageReference.error) {
-      if (retry > 0) return deleteImageReferenceFromDB(imageID, retry - 1)
-      console.log('Failed to delete reference image from DB')
-    }
-    else {
-      console.log('success delete reference image from DB')
-      return true
-    }
-  }
-
-  setTimeout(async () => {
-    let execute = await deleteWrapper(imageID)
-    execute == false && console.log('failed delete from storage and DB')
-  }, timeOut);
-  res.json({ message: 'success!' })
+  }, 5000);
 }
 
+const deleteImageFromStorage = async (fileName, retry = 3) => {
+  let deleteImage = await supabase.storage.from(storageName).remove([fileName])
+  // console.log(deleteImage)
+
+  if (deleteImage.error) {
+    if (retry > 0) return deleteImageFromStorage(fileName, retry - 1)
+    console.log('Failed to delete image from storage')
+  }
+  else {
+    console.log('success delete image from storage')
+    return true
+  }
+}
+
+const deleteImageReferenceFromDB = async (imageID, retry = 3) => {
+  let deleteImageReference = await supabase.from('temp_storage').delete().eq('file_name', imageID)
+  // console.log(deleteImageReference)
+
+  if (deleteImageReference.error) {
+    if (retry > 0) return deleteImageReferenceFromDB(imageID, retry - 1)
+    console.log('Failed to delete reference image from DB')
+  }
+  else {
+    console.log('success delete reference image from DB')
+    return true
+  }
+}
